f35c8878bb3bf0d3cdacaeac6716bd5a4b8b0595
imamsyarifm <imamsyarifm@gmail.com>
2022-04-06 23:30:51 +0700

    udpate revise

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
       Timer(
           const Duration(seconds: 3),
           () => Navigator.pushReplacementNamed(
-              context, (finalToken == 'null') ? '/login' : '/follow_up'));
+              context, (finalToken == 'null') ? '/login' : '/dashboard'));
     });
   }
 
